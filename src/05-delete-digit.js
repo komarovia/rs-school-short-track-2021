4f61df8a4dcbecefd4987fdd597de358da388827
komarovia <komarovia@gmail.com>
2021-04-09 00:11:57 +0300

    feat: add solution 05-delete-digit

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,36 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const arrTest = String(n).split('');
+  const arrPrime = [];
+
+  if (arrTest.includes('0')) {
+    arrTest.splice(arrTest.findIndex((item) => item === '0'), 1);
+    if (arrTest.length <= 3) {
+      return +arrTest.join('');
+    }
+  }
+  if (arrTest[0] === '1' && +arrTest[1] > +arrTest[0]) {
+    arrTest.splice(0, 1);
+    if (arrTest.length < 3 && arrTest[0] > arrTest[1]) {
+      return +arrTest.join('');
+    }
+  }
+
+  for (let i = 0; i < arrTest.length; i++) {
+    if (arrTest.includes(arrTest[i], i + 1)
+      || (!arrTest.includes(arrTest[i], i + 1)
+      && arrTest[i] === arrPrime[arrPrime.length - 1])) {
+      arrPrime.push(arrTest[i]);
+      arrTest.splice(i, 1);
+      i -= 1;
+    }
+  }
+  arrTest.reverse();
+  arrPrime.push(arrTest[0]);
+
+  return +arrPrime.join('');
 }
 
 module.exports = deleteDigit;
