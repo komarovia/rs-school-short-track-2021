b48446d09b08754b69408b11d50f1741211a4a7d
komarovia <komarovia@gmail.com>
2021-04-11 13:55:18 +0300

    feat: add solution 08-remove-from-list

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -17,8 +17,21 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new Error('Not implemented');
+function removeKFromList(l, k) {
+  let list = l;
+  let advanceList = l;
+
+  while (list.value === k) {
+    list = list.next;
+  }
+  while (advanceList.next) {
+    if (advanceList.next.value === k) {
+      advanceList.next = advanceList.next.next;
+    } else {
+      advanceList = advanceList.next;
+    }
+  }
+  return list;
 }
 
 module.exports = removeKFromList;
